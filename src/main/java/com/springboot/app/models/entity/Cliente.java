708ce1db86670c9763b129444e6e9bdace6ac22d
JuanJo Rey <juanreyfotos@gmail.com>
2021-08-31 16:56:36 +0200

    form

--- src/main/java/com/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/springboot/app/models/entity/Cliente.java
@@ -13,6 +13,8 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 @Table(name = "clientes")
 public class Cliente implements Serializable {
@@ -27,14 +29,15 @@ public class Cliente implements Serializable {
 
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 
 	private static final long serialVersionUID = 1L;
 	
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
+//	@PrePersist
+//	public void prePersist() {
+//		createAt = new Date();
+//	}
 
 	public Long getId() {
 		return id;
