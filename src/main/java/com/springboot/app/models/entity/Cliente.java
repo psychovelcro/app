91210d9d1ae533bc7a305c6f028e5efe92438c20
JuanJo Rey <juanreyfotos@gmail.com>
2021-09-01 12:07:17 +0200

    messages.properties

--- src/main/java/com/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/springboot/app/models/entity/Cliente.java
@@ -8,10 +8,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -23,10 +26,18 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotEmpty 
+//	@Size(min = 4, max = 12 )
 	private String nombre;
+	
+	@NotEmpty 
 	private String apellido;
+	
+	@NotEmpty 
+	@Email
 	private String email;
 
+	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
